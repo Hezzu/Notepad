f295d94b9ef5e099017070fdce0358f59aca399f
Hezzu <80692263+Hezzu@users.noreply.github.com>
2022-03-25 10:11:29 +0100

    Delete Notepad/src/main/kotlin directory

--- Notepad/src/main/kotlin/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.notepad {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires kotlin.stdlib;
-
-
-    opens com.example.notepad to javafx.fxml;
-    exports com.example.notepad;
-}
